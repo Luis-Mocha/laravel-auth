1f377d34cbb9fee2834c13c5b8684afc8d8696c9
Luis-Mocha <luismocha97@gmail.com>
2023-06-20 22:28:16 +0200

    padding jumbo

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="jumbotron p-5 mb-4 bg-light rounded-3">
+<div class="jumbotron p-3 mb-4 bg-light rounded-3">
     <div class="container py-5">
 
+        <span class="fw-bold fs-4">Modifica del progetto:</span>
         <h1 class="display-5 fw-bold">
-            Modifica del progetto
+            {{$project['title']}}
         </h1>
     </div>
 </div>
