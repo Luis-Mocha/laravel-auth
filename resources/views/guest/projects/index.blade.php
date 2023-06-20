ac8e435aae04475fb726254dbd460c4e44f2071a
Luis-Mocha <luismocha97@gmail.com>
2023-06-20 22:30:04 +0200

    padding jumbo

--- resources/views/guest/projects/index.blade.php
+++ resources/views/guest/projects/index.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="jumbotron p-5 mb-4 bg-light rounded-3">
+<div class="jumbotron p-3 mb-4 bg-light rounded-3">
     <div class="container py-5">
 
         <h1 class="display-5 fw-bold">
-            My projects
+            Explore my projects
         </h1>
     </div>
 </div>
